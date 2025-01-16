a0d3c89ccd6f05b3747a76e25f257083bee8ee9f
sake <sake.std.co@gmail.com>
2025-01-16 21:08:32 +0900

    UI作成 - gluestack-ui v1  のインストール

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerTintColor: '#000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+    <GluestackUIProvider config={config}>
+      <Stack screenOptions={{ headerTintColor: '#000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
 
-      {/* ホーム */}
-      <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
+        {/* ホーム */}
+        <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
 
-      {/* ラベル */}
-      <Stack.Screen name="labels" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
+        {/* ラベル */}
+        <Stack.Screen name="labels" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
 
-      {/* メモ */}
-      <Stack.Screen name="memos/index" options={{ headerTitle: 'メモ' }} />
-      <Stack.Screen name="memos/create" options={{ headerTitle: '' }} />
-      <Stack.Screen name="memos/[id]" options={{ headerTitle: '' }} />
-    </Stack>
+        {/* メモ */}
+        <Stack.Screen name="memos/index" options={{ headerTitle: 'メモ' }} />
+        <Stack.Screen name="memos/create" options={{ headerTitle: '' }} />
+        <Stack.Screen name="memos/[id]" options={{ headerTitle: '' }} />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
